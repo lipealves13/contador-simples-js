c3c57cf372f5a4d195fb6f108cd747d46bfab273
lipealves22 <lipealves22@gmail.com>
2022-01-09 00:27:47 -0300

    confirmar em 'main'

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
+
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
