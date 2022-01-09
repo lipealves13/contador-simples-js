e832af8890648d8c6e0117b1d46a464da1e02778
lipealves22 <lipealves22@gmail.com>
2022-01-09 00:25:30 -0300

    first
    adicionando arquivos ao repositório

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
